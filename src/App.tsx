336fbce4f7f158ec1553e0b9fe4ec5a0e2530efa
jake <jsmith181997@gmail.com>
2025-03-08 12:04:15 -0600

    chore: adjust config, add shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import "./index.css"
 import {useState} from "react";
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
 
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className={"bg-slate-300"}>
       <p>Number of times clicked: {count}</p>
-      <button onClick={() => {
+      <Button onClick={() => {
         setCount((prevCount) => prevCount +1)
-      }}>Click Me</button>
+      }}>Click Me</Button>
+
     </div>
   )
 }
